63114b58dfd523fdc6c66483787c94ae679743ae
Noprizal <noprizal@voltras.co.id>
2025-10-20 15:10:12 +0700

    fix

--- public/build/assets/loader-circle-Nb5mlMwp.js
+++ public/build/assets/loader-circle-Nb5mlMwp.js
@@ -1,4 +1,4 @@
-import{a as e}from"./createLucideIcon-DNdFhGNb.js";/**
+import{a as e}from"./createLucideIcon-CPyi91cO.js";/**
  * @license lucide-react v0.475.0 - ISC
  *
  * This source code is licensed under the ISC license.
